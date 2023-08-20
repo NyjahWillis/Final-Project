0cc073df64a5ca8b682f04fb1f5cea4128b31e26
NyjahWillis <Nyjah.Willis@emory.edu>
2023-08-20 03:02:56 -0400

    Added age_cat variable

--- Final Project.R
+++ Final Project.R
@@ -7,11 +7,20 @@ remotes::install_github("higgi13425/medicaldata")
 
 Smart <- medicaldata::smartpill
 
-smart_cat<- Smart |>
+smart_cc<- Smart |>
 		mutate (group_cat = factor(Group, labels = c("Critical Patient", "Healthy Volunteer")),
 						sex_cat = factor(Gender, labels = c("Female", "Male")),
 						race_cat = factor(Race, labels = c("White", "Black", "Asian/Pacific Islander", "Hispanic", "Other")))
-count(smart_cc, race_cat)
 
-smart_cc <- na.omit(smart_cat)
+smart_data <- na.omit(smart_cat)
+
+list(smart_data$Age)
+quantile(smart_data$Age, probs = c(.25, .75))
+smart_data$Age_cat <- factor(ifelse(is.na(smart_data$Age),NA,
+																			 ifelse(smart_data$Age <=28, 1,
+																			 			 ifelse(smart_data$Age <= 44, 2, 3))),
+																levels=1:3,
+																labels=c("28 years or younger",
+																				 "Between 29 - 43 years",
+																				 "44 and older "))
 
